a818e9eff2437f1c335c5c98ebc808838373fa06
gxcgh <sathwikbc.brigade@gmail.com>
2026-02-23 14:58:18 +0530

    1st Try

--- prog1.java
+++ prog1.java
@@ -4,5 +4,7 @@ class prog1 {
     public static void main(String args[])
     {
         System.out.println("Trying to do something");
+        System.out.println("Now this will be in the branch");
+        System.out.println("Checking if jar is generated");
     }
 } 
